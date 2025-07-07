ed65563951325fe9360aba4b4ab0a2af19ef5c78
masabkhan-cyber <75547252+masabkhan-cyber@users.noreply.github.com>
2025-07-08 01:38:40 +0500

    Easy Installer batch file

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,10 +1,10 @@
 @echo off
 echo Installing Python package dependencies...
-echo This may take a few minutes.
-pip install 
+echo This may take a few minutes. 
 echo.
 echo Installing local project from setup.py...
-pip install .
+python -m pip install .
+python -m pip install -r requirements.txt
 echo.
 echo Installation complete! You can close this window.
 pause
